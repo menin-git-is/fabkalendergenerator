af69145403bb1c7b766374f26240adb2d89a05a4
Holger Hellmuth <hellmuth@ira.uka.de>
2021-11-01 16:19:58 +0100

    Support auch für garantierte Einzelblöcke am Abend, help als Parameter

--- fabkalendergenerator.py
+++ fabkalendergenerator.py
@@ -10,15 +10,17 @@ import locale
 
 weekdays=['Montag','Dienstag','Mittwoch','Donnerstag','Freitag','Samstag','Sonntag']
 
-# 1== Tag ist normal gelistet, 2== Tag ist hervorgehoben
-daystoprint=[1,2,1,2,1,1,0]
+# 1== Tag ist normal gelistet, 2== Tag ist hervorgehoben 3== Erster Block ist hervorgehoben
+daystoprint=[1,3,3,3,1,1,0]
 
-
+############################
 
 def header (monat, jahr):
     print (f'\n======  Öffnungstage {datetime.date(jahr,monat,1).strftime("%B")} {jahr} ======\n')
     print ('[[:labsitter:intern:tagesliste_regeln|Regeln]]\n')
 
+############################
+    
 def tabelle (monat, jahr):
     donnerstage=0
 
@@ -36,28 +38,37 @@ def tabelle (monat, jahr):
         if daystoprint[wochentag]==1:
             out += (f"|{tag}.{monat}.|{weekdays[wochentag]}| | | |\n")
         if daystoprint[wochentag]==2:
-            out += (f"|**{tag}.{monat}.**|**{weekdays[wochentag]}**| | |**OFFEN** |\n")
+            out += (f"|**{tag}.{monat}.**|**{weekdays[wochentag]}**^ ^ | |\n")
+        if daystoprint[wochentag]==3:
+            out += (f"|**{tag}.{monat}.**|**{weekdays[wochentag]}**^ | | |\n")
     out += ("\n\n")
     print(out)
     return out
+
+
+
+#################################
 
 def main(argv):
 
     locale.setlocale(locale.LC_ALL, '')
     try:
-        opts, optargs = getopt.getopt(argv,"", ["monat=","jahr="])
+        opts, optargs = getopt.getopt(argv,"", ["monat=","jahr=","help"])
 
     except getopt.ParameterError:
         sys.exit(2)
 
     monat=jahr=''
+    help=False
     for opt, arg in opts:
         if opt in ("--monat") :
             monat= arg
         if opt in ("--jahr") :
             jahr= arg
+        if opt in ("--help") :
+            help= True
 
-    if monat=='' and jahr=='':
+    if (monat=='' and jahr=='') or help:
         print("\nlabkalendergenerator --monat=nn --jahr=nnnn\n\n"
 	      "          monat  Monatnummer 1-12\n"
 	      "          jahr   Das Jahr\n")
